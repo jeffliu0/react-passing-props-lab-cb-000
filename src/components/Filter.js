f3f9f32006e9aa0b1aba0b32c8da541d929872ac
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-06 22:15:01 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
+<<<<<<< HEAD
 const Filter = ({filters, handleChange}) => (
   <select onChange={handleChange} defaultValue='all'>
         <option value='all'>All</option>
         {filters.map(filter =>
+=======
+const Filter = ({filters}) => (
+  <select onChange={this.props.handleChange} defaultValue='all'>
+        <option value='all'>All</option>
+        {this.props.filters.map(filter =>
+>>>>>>> 8640bb45e5389dd3356f4cf6feb8f9729fc19a55
           <option key={filter} value={filter}>{filter}</option>
         )}
   </select>
 )
 
+<<<<<<< HEAD
 Filter.defaultProps ={
   filters: [],
   handleChange: () => {}
 }
+=======
+>>>>>>> 8640bb45e5389dd3356f4cf6feb8f9729fc19a55
 export default Filter
--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+<<<<<<< HEAD
 const FilteredFruitList = ({ fruit, filter }) => {
   const fruitList = !filter ?  fruit : fruit.filter(item => item.fruit_type === filter);
 
@@ -14,5 +15,12 @@ FilteredFruitList.defaultProps = {
   fruit: [],
   filter: null
 };
+=======
+const FilteredFruitList = ({list}) => (
+     <ul className="fruit-list">
+        {list.map((item, index) => <li key={index}>{item.char}</li>)}
+      </ul>
+)
+>>>>>>> 8640bb45e5389dd3356f4cf6feb8f9729fc19a55
 
 export default FilteredFruitList;
